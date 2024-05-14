e7dad6d786181b96444ade6d0fb33c6476bf51b6
Mark <33894478+IMKAOS23@users.noreply.github.com>
2024-05-14 10:46:47 +0100

    Update OwnerInfo.java

--- VeterinaryApplication/src/main/java/com/mycompany/veterinaryApplication/components/OwnerInfo.java
+++ VeterinaryApplication/src/main/java/com/mycompany/veterinaryApplication/components/OwnerInfo.java
@@ -12,22 +12,40 @@ public class OwnerInfo {
     private int id;
     private String identifier; // Either Email or Business/Farm Name
 
+    /**
+     * Constructor creates an OwnerInfo object to be used within a ComboBox
+     * 
+     * @param id
+     * @param identifier 
+     */
     public OwnerInfo(int id, String identifier) {
         this.id = id;
         this.identifier = identifier;
     }
 
+    /**
+     * Getter for the ID of the OwnerInfo
+     * 
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter for the Identifier of the OwnerInfo. Usually an Email or Business Name
+     * @return 
+     */
     public String getIdentifier() {
         return identifier;
     }
 
-    // Overriding the ComboBox toString method this will only show indentifier instead of object.
-    // This is an Email for Pet Owner or Vet
-    // And Name of Business for Business
+    /**
+     * Method overwrites the toString method of the String class to change what is found within the ComboBox.
+     * This then changes what is written with the ComboBox. In this case to the Identifier that is the Email or Business Name
+     * 
+     * @return identifier
+     */
     @Override
     public String toString() {
         return identifier;
